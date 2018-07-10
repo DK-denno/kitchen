8d11e4b97f1eee1b120284271c7066d0a8582649
dk-denno <dennisveer27@gmail.com>
2018-07-10 10:32:30 +0300

    added customers photos

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#submit").click(function(){
+    return alert("Thank you for subscribing");
+  });
+});
+// $(document).ready(function){
+//   $('#gallery').click(function(){
+//     return window.open('../gallery.html')
+//   });
+// });
